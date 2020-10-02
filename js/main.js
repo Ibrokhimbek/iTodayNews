326da03e976f94c1378ea924e2b46545be2d6375
Ibrokhimbek <ibrokhim1348@gmail.com>
2020-10-02 18:05:24 +0500

    User actions' animations were done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+const burger = document.querySelector("#user_action_menu");
+const exitBtn = document.querySelector(".exit_btn");
+const menuArea = document.querySelector(".top_menu_area");
 
+burger.addEventListener("click", () => {
+  menuArea.classList.remove("navbar_hide");
+  menuArea.classList.add("navbar_open");
+});
+exitBtn.addEventListener("click", () => {
+  menuArea.classList.add("navbar_hide");
+  menuArea.classList.remove("navbar_open");
+});
+
+const searchArea = document.querySelector(".top_search_area");
+const search = document.querySelector("#user_action_search");
+const searchExit = document.querySelector("#searchExit");
+
+search.addEventListener("click", () => {
+    searchArea.classList.remove("navbar_hide")
+    searchArea.classList.add("navbar_open")
+});
+searchExit.addEventListener("click", () => {
+    searchArea.classList.remove("navbar_open")
+    searchArea.classList.add("navbar_hide")
+});
